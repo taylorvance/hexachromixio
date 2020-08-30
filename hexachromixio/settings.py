4c1501bdc0c6d412c7c747168eaa77d201d02628
Taylor Vance <tmax8908@yahoo.com>
2020-08-30 12:00:30 -0500

    Root templates and static folders, and base template

--- hexachromixio/settings.py
+++ hexachromixio/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'hexachromixio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
