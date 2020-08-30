4a5a7c95925b6b121ef65f7f4daae64971b4365b
Taylor Vance <tmax8908@yahoo.com>
2020-08-30 12:15:57 -0500

    Rearranged app template folder

--- hexachromix/views.py
+++ hexachromix/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def landing_page(request):
-    return render(request, 'landing_page.html')
+    return render(request, 'hexachromix/landing_page.html')
