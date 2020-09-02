968058532f2a4b79601aeed109e6c92b4c3a2162
Taylor Vance <tmax8908@yahoo.com>
2020-09-02 15:34:37 -0500

    Full template for rules, rearranged some urls, added favicon

--- hexachromix/views.py
+++ hexachromix/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def landing_page(request):
     return render(request, 'hexachromix/landing_page.html')
+
+def create_or_join(request):
+    return HttpResponse("creating game "+request.POST['game_name'])
--- hexachromixio/urls.py
+++ hexachromixio/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hexachromix.urls')),
+
+    path('', TemplateView.as_view(template_name='landing_page.html')),
+    path('rules', TemplateView.as_view(template_name='rules.html')),
+
+    path('game/', include('hexachromix.urls')),
 ]
