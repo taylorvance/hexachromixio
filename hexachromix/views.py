b3091ab792eb4d819ab79c59772a9c1ed72c3f60
Taylor Vance <tmax8908@yahoo.com>
2020-09-22 19:41:17 -0500

    Require login for game creation

--- hexachromix/views.py
+++ hexachromix/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from hexachromix.models import Game
 
 
+def find_game(request):
+    game_uid = request.GET['game_uid']
+    game_uid = game_uid.strip()
+    game_uid = game_uid.lower()
+
+    try:
+        game = Game.objects.get(uid=game_uid)
+    except Game.DoesNotExist:
+        return HttpResponse("game %s not found :(" % game_uid)
+
+    return redirect('/game/%s' % game.uid)
+
+@login_required
 def create_game(request):
-    game = Game(variant=request.POST['variant'])
+    game = Game(variant=Game.Variant[request.POST['variant']])
+    game.author = request.user
     game.save()
 
     return redirect('/game/%s' % game.uid)
@@ -17,4 +31,7 @@ def view_game(request, game_uid):
     except Game.DoesNotExist:
         return HttpResponse("game %s not found :(" % game_uid)
 
-    return HttpResponse("here's game %s (id %d)" % (game.uid, game.pk))
+    if game.is_active:
+        return render(request, 'hexachromix/play.html', {'game': game})
+    else:
+        return render(request, 'hexachromix/view_game.html', {'game': game})
